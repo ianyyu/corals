5960e13e4b281e37ea54ce19ee3daf625fc2722b
Ian Yu <ian97yu@gmail.com>
2024-10-13 19:42:06 -0400

    inital code

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-import scrollama from "scrollama"; // or...
-const scrollama = require("scrollama");
 // Initialize Scrollama
 const scroller = scrollama();
 
 // Setup scroller
 scroller
   .setup({
-    step: ".step", // Class of the step elements
-    offset: 0.5,   // The trigger point is when step reaches the middle of the viewport
-    debug: true,   // Shows trigger points while developing
+    step: ".scroll-section",   // Step elements
+    offset: 0.5,               // Trigger point
+    debug: true                // Debugger
   })
   .onStepEnter((response) => {
-    // Actions when step enters view
     const { element } = response;
-    element.style.backgroundColor = '#add8e6'; // Change background color on enter
-    console.log(`Entered step: ${element.dataset.step}`);
+    console.log(`Entering step: ${element.id}`);
   })
   .onStepExit((response) => {
-    // Actions when step leaves view
-    const { element } = response;
-    element.style.backgroundColor = '#f4f4f4'; // Reset background color on exit
-    console.log(`Exited step: ${element.dataset.step}`);
+    console.log(`Exiting step: ${response.element.id}`);
   });
